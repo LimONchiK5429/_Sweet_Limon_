58e7afa2f579612f6ea722f9007ad924e8409b1a
LimONchiK5429 <137615490+LimONchiK5429@users.noreply.github.com>
2023-07-22 17:01:06 +0500

    ruenTranslater.py

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-print('hello world')
-
-
-tydtyd.gfhg
-str
-str'V'B
-    GF'D' WarningFunc   Warning SyntaxWarning
+#
+#hello today I am publishing a code to correct errors, I allow you to use my code to create applications, 
+#the code works in Russian and English layout for convenience, you can change languages, 
+#Just replace the letters with your language, it works something like this:
+ #   "Ghbdtn Fylhtq rfr ltkf" "Привет андрей как дела" or "Рш пгны рщц фку нщг" "Hi guys how are you"
+#Have a good project!
+ru = list("йцукенгшщзфывапролдячсмитьхъжэбю ")
+en = list("qwertyuiopasdfghjklzxcvbnm[];',. ")
+ru_en = {}
+en_ru = {}
+for i in range(len(ru)):
+    ru_en[ru[i]] = en[i]
+    en_ru[en[i]] = ru[i]
+inp = input('Enter the text with confused layout:')
+out = []
+for i in inp:
+    if i in en:
+        out.append(en_ru[i])
+    elif i in ru:
+        out.append(ru_en[i])
+print(''.join(out))
+#and he does not translate dots either, because in Russian there are more letters than in English Buy-Buy!
